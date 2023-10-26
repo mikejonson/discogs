a77d85f71a7d5aac19f18e739588839912dd1658
mikejonson <149112057+mikejonson@users.noreply.github.com>
2023-10-26 23:08:11 +0300

    Create discogs.js

---
+++ discogs.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+$('body').append('<style>.added_releases{background:#b1cfb1;}</style>');
+
+var tdElements = document.getElementsByClassName('releases_8o12o');
+  
+for (var i = 0; i < tdElements.length; i++) {
+  tdElements[i].addEventListener('click', function() {
+    if (this.classList.contains('added_releases')) {
+      console.log('release removed from database');
+      this.classList.remove('added_releases');
+    } else {
+      console.log('release added to database');
+      this.classList.add('added_releases');
+    }
+  });
+}
+});
